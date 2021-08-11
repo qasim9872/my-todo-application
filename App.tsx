65ee8a16b188ea70d63572d4dd810b94831ad6e2
Muhammad Qasim <qasim9872@gmail.com>
2021-08-11 20:23:45 +0100

    refactoring the app file

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 import { useFonts } from "./src/infrastructure/core/fonts";
 import tw from "./src/styles/tailwind";
+import { Home } from "./src/pages/home/home.page";
 
 export default function App() {
   const fontsLoaded = useFonts();
@@ -13,10 +14,8 @@ export default function App() {
   }
 
   return (
-    <View style={tw`flex-1 bg-red-100 items-center justify-center`}>
-      <Text style={tw`text-black font-poppins font-bold`}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <View style={tw`flex-1`}>
+      <Home />
       <ExpoStatusBar style="auto" />
     </View>
   );
